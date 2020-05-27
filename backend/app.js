7910ac497224ac53b2c8332b24e041f0ac029766
italo <contato.italo1020@gmail.com>
2020-05-27 14:16:36 -0300

    finalizacao do projeto

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const restful = require('node-restful');
+const bodyParse = require('body-parser');
+const cors = require('cors');
+
+const server = express();
+const mongoose = restful.mongoose;
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://db/mydb');
+
+server.use(bodyParse.urlencoded({extended: true}));
+server.use(bodyParse.json());
+server.use(cors());
+
+const Client = restful.model(('Client'), {
+    name: {
+        type: String,
+        required: true}
+});
+Client.methods(['get', 'post', 'put', 'delete']);
+Client.updateOptions({new: true, runValidators: true});
+Client.register(server, '/clients');
+server.listen(3000);
